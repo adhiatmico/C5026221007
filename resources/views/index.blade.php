58f4e7d8550ce45f5f89c3bed083d10a50b09ab7
adhiatmico <adhiatmico@gmail.com>
2023-12-03 13:17:38 +0700

    .

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,42 +1,43 @@
 @extends('master2')
-@section('judulhalaman', 'Data Pegawai')
+@section('judulhalaman','Data Pegawai')
 @section('konten')
-	<h2>www.malasngoding.com</h2>
-	<h3>Data Pegawai</h3>
+<h2>www.malasngoding.com</h2>
+<h3>Data Pegawai</h3>
 
-	<a href="/pegawai/tambah" class="btn btn-primary"> + Tambah Pegawai Baru</a>
+<a href="/pegawai/tambah" class="btn-primary"> + Tambah Pegawai Baru</a>
 
-	<br/>
+    <br/>
     <p>Cari Data Pegawai :</p>
-	<form action="/pegawai/cari" method="GET">
-		<input class="form-control" type="text" name="cari" placeholder="Cari Pegawai .."
-        value="{{ old("cari", isset($cari) ? $cari : '') }}">
-		<input type="submit" value="CARI" class= "btn btn-info">
-	</form>
-	<br/>
+        <form action="/pegawai/cari" method="GET">
+            <input class="form-control" type="text" name="cari" placeholder="Cari Nama Pegawai .."
+            value="{{ old("cari", isset($cari) ? $cari : '') }}">
+            <input type="submit" value="CARI" class="btn btn-info">
+        </form>
+    <br/>
 
-	<table class="table table-striped table-hover">
-		<tr>
-			<th>Nama</th>
-			<th>Jabatan</th>
-			<th>Umur</th>
-			<th>Alamat</th>
-			<th>Opsi</th>
-		</tr>
-		@foreach($pegawai as $p)
-		<tr>
-			<td>{{ $p->pegawai_nama }}</td>
-			<td>{{ $p->pegawai_jabatan }}</td>
-			<td>{{ $p->pegawai_umur }}</td>
-			<td>{{ $p->pegawai_alamat }}</td>
-			<td>
-				<a href="/pegawai/edit/{{ $p->pegawai_id }}" class="btn btn-warning">Edit</a>
-				|
-				<a href="/pegawai/hapus/{{ $p->pegawai_id }}" class="btn btn-danger">Hapus</a>
-			</td>
-		</tr>
-		@endforeach
-	</table>
-    {{$pegawai-> links()}}
+    <table class="table table-striped tabel-hover">
+            <tr>
+                <th>Nama</th>
+                <th>Jabatan</th>
+                <th>Umur</th>
+                <th>Alamat</th>
+                <th>Opsi</th>
+            </tr>
+        @foreach($pegawai as $p)
+        <tr>
+            <td>{{ $p->pegawai_nama }}</td>
+            <td>{{ $p->pegawai_jabatan }}</td>
+            <td>{{ $p->pegawai_umur }}</td>
+            <td>{{ $p->pegawai_alamat }}</td>
+            <td>
+            <a href="/pegawai/view/{{ $p->pegawai_id }}" class="btn btn-success">View</a>
+            |
+            <a href="/pegawai/edit/{{ $p->pegawai_id }}" class="btn btn-warning">Edit</a>
+            |
+            <a href="/pegawai/hapus/{{ $p->pegawai_id }}" class="btn btn-danger">Hapus</a>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+        {{ $pegawai-> links()}}
     @endsection
-
--- resources/views/tambah.blade.php
+++ resources/views/tambah.blade.php
@@ -1,10 +1,7 @@
-<html>
-<head>
-	<title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
-</head>
-<body>
-
-	<h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
+@extends('master2')
+@section('judulhalaman', 'Data Pegawai')
+@section('konten')
+    <h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
 	<h3>Data Pegawai</h3>
 
 	<a href="/pegawai"> Kembali</a>
@@ -12,14 +9,24 @@
 	<br/>
 	<br/>
 
-	<form action="/pegawai/store" method="post">
+	<form action="/pegawai/store" method="post" class="form-horizontal">
 		{{ csrf_field() }}
+        <div class="form-group row">
+            <label for="nama" class="col-xs-3 col-form-label mr-2">Nama</label>
+            <div class="col-xs-9">
+                <input type="text" class="form-control" id="nama" name="nama">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="nama" class="col-xs-3 col-form-label mr-2">Jabatan</label>
+            <div class="col-xs-9">
+                <input type="text" class="form-control" id="nama" name="nama">
+            </div>
+        </div>
 		Nama <input type="text" name="nama" required="required"> <br/>
 		Jabatan <input type="text" name="jabatan" required="required"> <br/>
 		Umur <input type="number" name="umur" required="required"> <br/>
 		Alamat <textarea name="alamat" required="required"></textarea> <br/>
-		<input type="submit" value="Simpan Data">
+		<input type="submit" value="Simpan Data" class="btn btn-primary">
 	</form>
-
-</body>
-</html>
+@endsection
